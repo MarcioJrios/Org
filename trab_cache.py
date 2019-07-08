71baf4ff40e00b111f467fd8a4d40acccdfdbb00
MarcioJrios <31110984+MarcioJrios@users.noreply.github.com>
2019-07-07 21:53:41 -0300

    Add files via upload

--- trab_cache.py
+++ trab_cache.py
@@ -4,7 +4,7 @@ num_cel = 128
 tam_bloco_mp = 4 # K
 num_l_cache = 8
 tam_cel = 8
-mem_p = []
+mem_p = [] # lista que simula a memoria principal
 deslocamento = 2
 
 # Numero do bloco = endereço / K
@@ -12,10 +12,14 @@ deslocamento = 2
 cache = [] # [()] lista de tuplas que guardara qual bloco e qual o deslocamento ha em cada linha (quadro(linha), num. bloco, deslocamento)
 cache_lines = [] # [[]] lista que armazena o bloco em cada linha da cache
 
-def dec_to_bin(x):
+def dec_to_bin(x): #transforma o numero dec em uma string em binario
     return str(bin(x))
 
-def gerate_mp():
+def calc_block(x): #calcula o endereco da primeira celula associada ao bloco x
+	endco = x * tam_bloco_mp
+	return endco
+
+def gerate_mp(): #preenche a MP com valores aleatorios de 8 bits
 	i = 0
 	while(i < 128):
 		item  = randint(0, 255)
@@ -40,31 +44,47 @@ def gerate_mp():
 			mem_p.append(item)
 		i = i+1
 
+def get_block(endco): #busca as 4 celulas de um bloco
+	lista = []
+	h = 0
+	while(h < 4):
+		lista.append(mem_p[endco+h])
+		h = h+1
+	return lista
+
+def fill_cache(): #preenche a cache inicialmente com os 8 blocos iniciais da MP
+	i = 0
+	while(i < 8):
+		endereco = calc_block(i)
+		num = dec_to_bin(endereco)
+		size = len(num) - 2 #tam da string item sem o '0b'
+		j = 0
+		count =0
+		while(j < size):
+			count = count+1
+			j = j+1
+		if count ==  8:
+			num = num.replace('0b', '')
+			cache.append((i, num, '00'))
+			lista = get_block(endereco)
+			cache_lines.append(lista)
+		else:
+			aux = tam_cel - count - 2
+			j = 0
+			zeros = ''
+			while j<aux:
+				zeros = zeros+'0'
+				j = j+1
+			num = num.replace('0b', zeros)
+		cache.append((i, num, '00'))
+		lista = get_block(endereco)
+		cache_lines.append(lista)
+		i = i+1
+
 gerate_mp()
 
-i = 0
-while(i < 8):
-	endereco = i * tam_bloco_mp
-	num = dec_to_bin(endereco)
-	size = len(num) - 2 #tam da string item sem o '0b'
-	j = 0
-	count =0
-	while(j < size):
-		count = count+1
-		j = j+1
-	if count ==  8:
-		num = num.replace('0b', '')
-		cache.append((i, num, '00'))
-	else:
-		aux = tam_cel - count - 2
-		j = 0
-		zeros = ''
-		while j<aux:
-			zeros = zeros+'0'
-			j = j+1
-		num = num.replace('0b', zeros)
-	cache.append((i, num, '00'))
-	i = i+1
+fill_cache()
+
 
 print(cache)
-print(mem_p)
+print(cache_lines)
